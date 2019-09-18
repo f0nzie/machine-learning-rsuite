f1c59d129518b0005308005ded3b7f641a57e9f7
f0nze zbook15g2 <alfonso.reyes@oilgainsanalytics.com>
2019-09-18 16:33:34 -0500

    update, remove, reorder

---
+++ work/one_book_at_a_time/_common.R
@@ -0,0 +1,20 @@
+set.seed(1014)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = FALSE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618 #,  # 1 / phi
+  # fig.show = "hold"
+  # message = FALSE,
+  # error = TRUE,
+  # warning = FALSE,
+)
+
+# knitr::opts_knit$set(root.dir = normalizePath(assets_dir))
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
