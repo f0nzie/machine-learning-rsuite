e57f33d92993e22edf32c8232aad977dc85055ef
f0nze zbook15g2 <alfonso.reyes@oilgainsanalytics.com>
2019-09-18 16:12:23 -0500

    symbolic links to output PDF

---
+++ baked/copy_pdf_syms.sh
@@ -0,0 +1,9 @@
+ln -sf ../work/book-algorithms_comparison/_book/book-algorithms_comparison.pdf .
+ln -sf ../work/book-classification/_book/book-classification.pdf .
+ln -sf ../work/book-feature_engineering/_book/book-feature_engineering.pdf .
+ln -sf ../work/book-linear_regression-101/_book/book-linear_regression_101.pdf .
+ln -sf ../work/book-linear_regression-303/_book/book-linear_regression-303.pdf .
+ln -sf ../work/book-ml_meta/_book/book-ml_meta.pdf .
+ln -sf ../work/book-neural_networks/_book/book-neural_networks.pdf .
+
+
